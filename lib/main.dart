9d09612258ad7bb2623760c20e1cca725b63cd90
Dongchan Shin <dc.shin@kakaogames-us.com>
2024-10-10 00:42:29 +0900

    [Done] Part 2. 기초 / Ch 01.Flutter Widget 101 / 05.Container, Center Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("This is App bar"),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              print('Tab! Home');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.play_arrow),
-            onPressed: () {
-              print('Tab! Play');
-            },
-          ),
-        ],
-      ),
       body: MyApp(),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.bug_report),
-          onPressed: () {
-            print("Tab! FAB");
-          }),
     ),
   ));
 }
@@ -39,7 +16,8 @@ class MyApp extends StatelessWidget {
     return SafeArea(
       child: Center(
         child: Text(
-          style: TextStyle(color: Colors.black, fontSize: 60),
+          style: TextStyle(
+              color: Colors.black, fontSize: 40),
           "Hello, Flutter",
         ),
       ),
