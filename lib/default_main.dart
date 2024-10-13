b96bca9d5f682de79857a5dd6001684b4cf27b1b
Dongchan Shin <dc.shin@kakaogames-us.com>
2024-10-13 21:16:06 +0900

    [Done] Part 2. 기초 / Ch 07.Flutter Navigator / 02.페이지 이동하기 2

---
+++ lib/default_main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeWidget(),
+    ),
+  );
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
