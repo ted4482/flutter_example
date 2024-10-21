f1884b486b4c170977ff86f92eb8428cd8a557cf
Dongchan Shin <dc.shin@kakaogames-us.com>
2024-10-22 00:29:52 +0900

    [Done] Part 3. Flutter 활용 / Ch 01.흔들기 카운트앱 03.앱 이름,아이콘 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,125 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_example/insta_5/insta5_body.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(
-    const Insta5App()
-  );
+  runApp(const MyApp());
 }
 
-class Insta5App extends StatelessWidget {
-  const Insta5App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          primary: Colors.white,
-          secondary: Colors.black,
-        ),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.grey.shade600
-        ),
-        textTheme: const TextTheme(
-         bodyLarge: TextStyle(color: Colors.red),
-         bodyMedium: TextStyle(color: Colors.red),
-         bodySmall: TextStyle(color: Colors.red),
-        ),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const Insta5Home(),
+      home: const MyHomePage(title: 'Shake Count App'),
     );
   }
 }
 
-class Insta5Home extends StatefulWidget {
-  const Insta5Home({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  State<Insta5Home> createState() => _Insta5HomeState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _Insta5HomeState extends State<Insta5Home> {
-  late int pageIndex;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    pageIndex = 0;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: pageIndex == 0 ? AppBar(
-          title: Text('Instagram', style: GoogleFonts.lobsterTwo(fontSize: 32, color: Colors.black)),
-          actions: [
-            IconButton(onPressed: (){}, icon: const Icon(Icons.favorite_outline)),
-            IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.chat_bubble_text)),
-          ],
-        ) : null,
-        body: Insta5Body(pageIndex: pageIndex,),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (newIndex) => setState(() => pageIndex = newIndex),
-            currentIndex: pageIndex,
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-            ]),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '흔들어서 카운트를 올려보세요.',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
-/*
-- material, cupertino design 정의
-light, dark
- */
